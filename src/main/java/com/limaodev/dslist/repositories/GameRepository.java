9bc5009cb73eb7b84dd3dfb6173b7b68b48b654b
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-05-20 16:07:16 -0300

    Homolog ok

--- src/main/java/com/limaodev/dslist/repositories/GameRepository.java
+++ src/main/java/com/limaodev/dslist/repositories/GameRepository.java
@@ -12,7 +12,7 @@ public interface GameRepository extends JpaRepository<Game, Long>{
     
     @Query( nativeQuery = true, value = """
             
-            SELECT games.id, games.title, games.game_year AS `year`, games.img_url AS imgUrl, 
+            SELECT games.id, games.title, games.game_year AS gameYear, games.img_url AS imgUrl, 
                 games.short_description AS shortDescription, belongings.position
             FROM games
             INNER JOIN belongings ON games.id = belongings.game_id
